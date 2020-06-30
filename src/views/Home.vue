94d7ee348b207da50b605f5ca31e29db521b34be
getech <getech@getechdeMacBook-Air.local>
2020-06-30 19:41:40 +0800

    first commit

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,10 +2,12 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <van-button type="primary">默认按钮</van-button>
   </div>
 </template>
 
 <script>
+// import { Button } from 'vant'
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
@@ -13,6 +15,7 @@ export default {
   name: 'Home',
   components: {
     HelloWorld
+    // Button
   }
 }
 </script>
