16e087d1626d44f06844ac9eb7dc8781f1a738f2
getech <getech@getechdeMacBook-Air.local>
2020-07-01 13:40:19 +0800

    add:首页内容

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,21 +2,77 @@
   <div class="home">
 
     <van-button type="primary">默认按钮</van-button>
+    <van-list
+      v-model="loading"
+      :finished="finished"
+      finished-text="没有更多了"
+      @load="onLoad"
+    >
+      <van-cell v-for="item in list" :key="item" :title="item">
+        <div class="list">
+          <div class="left">
+            <img src="item.img" alt="">
+          </div>
+          <div class="right">
+            <div class="title">{{item.title}}</div>
+            <div class="create-time">{{item.createTime}}</div>
+          </div>
+        </div>
+      </van-cell>
+    </van-list>
 
   </div>
 </template>
 
 <script>
-import { Button, Tabbar, TabbarItem } from 'vant'
-// @ is an alias to /src
-// import HelloWorld from '@/components/HelloWorld.vue'
+import { Button, List, Cell } from 'vant'
 
 export default {
   name: 'Home',
   components: {
     [Button.name]: Button,
-    [Tabbar.name]: Tabbar,
-    [TabbarItem.name]: TabbarItem
+    [List.name]: List,
+    [Cell.name]: Cell
+  },
+  data () {
+    return {
+      loading: false,
+      finished: false,
+      list: []
+    }
+  },
+  methods: {
+    onLoad () {
+      setTimeout(() => {
+        this.loading = true
+        this.finished = true
+        this.list = []
+      }, 1000)
+    }
   }
 }
 </script>
+<style lang="less" scoped>
+.list{
+  display: flex;
+  flex-direction: row;
+  .left img{
+    width: 150px;
+    height: 100px;
+    border-radius: 10px;
+  }
+  .right{
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    margin-left: 15px;
+    .title{
+      font-size: 18px;
+    }
+    .create-time{
+      font-size: 12px;
+      color: #9e9e9e;
+    }
+  }
+}
+</style>
